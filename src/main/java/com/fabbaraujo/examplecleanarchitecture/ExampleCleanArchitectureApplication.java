08e1dcee9fcc738d86025e9ba4b4d12d118292ee
Fabrício Araújo <fabaraujo23@gmail.com>
2021-04-22 08:42:37 -0300

    Exemplo clean

---
+++ src/main/java/com/fabbaraujo/examplecleanarchitecture/ExampleCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.fabbaraujo.examplecleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleCleanArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleCleanArchitectureApplication.class, args);
+	}
+
+}
